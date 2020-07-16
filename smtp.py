9cda1e0cc7453136024b82facded9f139e2440eb
Bharath-varma-p <cogs132@gmail.com>
2020-07-17 03:10:45 +0530

    updated smtp

---
+++ smtp.py
@@ -0,0 +1,51 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Jul 17 21:54:29 2020
+
+@author: Bharath varma
+"""
+import smtplib, ssl
+from email.mime.text import MIMEText
+from email.mime.multipart import MIMEMultipart
+
+sender_email = "YYYYYY@gmail.com"
+receiver_email = "XXXXX@gmail.com"
+password = "abcd@2019"
+dr="BCC@gmail.com"
+#a=str(receiver_email[1])
+message = MIMEMultipart("alternative")
+message["Subject"] = "heyy "
+message["From"] = "YYYYYY@gmail.com"
+message["To"] = receiver_email
+
+# Create the plain-text and HTML version of your message
+text = """\
+HAI GITHUB"""
+html = """\
+<html>
+  <body>
+    <p>Hi,<br>
+    After a long time,Finally You are watching my progress Thank you,
+    </p>
+  </body>
+</html>
+"""
+
+# Turn these into plain/html MIMEText objects
+part1 = MIMEText(text, "plain")
+part2 = MIMEText(html, "html")
+
+# Add HTML/plain-text parts to MIMEMultipart message
+# The email client will try to render the last part first
+message.attach(part1)
+message.attach(part2)
+
+# Create secure connection with server and send email
+context = ssl.create_default_context()
+with smtplib.SMTP_SSL("smtp.gmail.com", 465, context=context) as server:
+    server.login("login@gmail.com", password) #note : it will only work if we allow less secure apps in google services 
+    server.sendmail(
+        sender_email,receiver_email, message.as_string()
+    )
+#happy learning......!
